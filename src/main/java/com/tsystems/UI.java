4102c5b66a8d7328245f75988c156f59ba2466a8
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-14 16:15:10 +0200

    Expandidas opciones del menú

--- src/main/java/com/tsystems/UI.java
+++ src/main/java/com/tsystems/UI.java
@@ -1,5 +1,6 @@
 package com.tsystems;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UI {
@@ -26,5 +27,37 @@ public class UI {
         return name;
     }
 
+    public void printMainMenu() {
+        MenuOptions.printMenu();
+    }
 
+    public void printError(Exception error) {
+        System.out.println("Se ha producido error. Motivo: " + error.getMessage());
+    }
+
+    public void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+
+    public void printWelcome() {
+        System.out.println("\n¡Hola! ¿Qué quieres hacer?");
+    }
+
+    public MenuOptions readMenuOption() throws InputMismatchException {
+        MenuOptions option = MenuOptions.NOOP;
+
+        try {
+            System.out.print("> ");
+            option = MenuOptions.fromValue(scan.nextInt());
+            scan.nextLine();
+        }
+        catch (InputMismatchException e) {
+            scan.nextLine();
+            throw new InputMismatchException("Introduce un comando válido.");
+        }
+
+        return option;
+    }
 }
