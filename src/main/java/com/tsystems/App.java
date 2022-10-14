4a41e438ec97f338215afacf1a947e518466ce00
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-14 16:15:22 +0200

    Mockup del menú principal

--- src/main/java/com/tsystems/App.java
+++ src/main/java/com/tsystems/App.java
@@ -1,13 +1,59 @@
 package com.tsystems;
 
+import java.util.InputMismatchException;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    private static UI ui = new UI();
+    private static CLInder clinder = new CLInder();
+
+    private static User user         = null;
+    private static UserDB userDB     = new UserDB();
+    private static UserAuth userAuth = new UserAuth(userDB);
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        var option = MenuOptions.NOOP;
+        Exception error = null;
+
+        do {
+            ui.clearScreen();
+
+            if (error != null) {
+                ui.printError(error);
+            }
+
+            error  = null;
+            option = MenuOptions.NOOP;
+
+            ui.printWelcome();
+            ui.printMainMenu();
+
+            try {
+                option = ui.readMenuOption();
+            }
+            catch (InputMismatchException e) {
+                error = e;
+            }
+
+
+            switch (option) {
+                case EXIT -> {
+
+                }
+                case LOGIN -> {
+
+                }
+                case REGISTER -> {
+
+                }
+            }
+
+        } while (option != MenuOptions.EXIT);
     }
 }
